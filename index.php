d1904c485dafe3ed2483a6e582ca34453e74cd56
SohailaKandil <133423333+SohailaKandil@users.noreply.github.com>
2023-12-31 21:48:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,41 @@
 <?php
-    include ("../db_connection.php");
+    include 'db_connection.php';
     $con = OpenCon();
-    session_start();
-    if (!isset($_SESSION["username"])) {
-        header("location: ../index.php");
-        exit(); // Make sure to exit after redirecting
+    $sql_products = "";
+    if (isset($_GET["no_boycott"])){
+        $sql_products = "SELECT DISTINCT p.product_image , p.product_name , p.product_id ,GROUP_CONCAT(DISTINCT s.nationality) AS nationalities
+        FROM products p , brands b , stakeholding st  ,shareholders s
+            WHERE p.brand_id = b.brand_id
+            AND b.brand_id = st.brand_id
+            AND st.shareholders_id = s.shareholder_id
+            
+        GROUP BY p.product_id
+        HAVING SUM(s.nationality NOT IN ('egypt', 'algeria', 'morocco', 'jordan', 'yemen', 'pakistan', 'palestine', 'malaysia', 'brazil')) = 0;
+        ";
     }
-    $name = $_SESSION["username"] ;
+    else{
+        $sql_products = "SELECT * FROM `products` WHERE 1";
+    }
+    
+    $result = $con -> query($sql_products);
 
-    $sql_rule = "SELECT rule FROM `users` WHERE user_name = '$name'";
-    $result_rule = $con -> query($sql_rule);
-    $result_rule = $result_rule->fetch_assoc();
-    if ($result_rule["rule"] == "user") {
-        header("location: ../home.php");
-        exit(); // Make sure to exit after redirecting
-    } 
+    $sql_ranges = "SELECT MIN(price) , MAX(price)
+    FROM (SELECT
+        price,
+        FLOOR(price / 10) * 10 AS range_start
+      FROM
+        products
+          )sub
+    GROUP BY range_start
+    ORDER BY range_start;";
+    $result_ranges = $con -> query($sql_ranges);
+
+    $sql_brands = "SELECT DISTINCT(brand_name) FROM brands";
+    $result_brands = $con -> query($sql_brands);
+
+    $sql_nat = "SELECT DISTINCT(nationality) FROM shareholders";
+    $result_nat = $con -> query($sql_nat); 
+    
 ?>
 
 <!DOCTYPE html>
@@ -22,72 +43,170 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <!-- bootstrap css link -->
+    <title>Grocery Store Website</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
     rel="stylesheet" 
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
     crossorigin="anonymous">
-    <!-- css file -->
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" 
+    integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" 
+    crossorigin="anonymous" 
+    referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- navbar -->
     <div class="container-fluid p-0">
-        <!-- First child -->
         <nav class="navbar navbar-expand-lg navbar-light bg-info">
             <div class="container-fluid">
-                <nav class="navbar navbar-expand-lg">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <h6 class="nav-link"> <?php echo "Welcome  $name" ?> </h6>
-                        </li>
-                        <li class="nav-item">
-                            <div class="button text-center mx-1">
-                                <a href="../log_out.php" class="btn btn-primary" ml-6>Log out</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <div class="button text-center mx-1">
-                                <a href="../home.php"  class="btn btn-primary" ml-6>Home page</a>
-                            </div>
-                        </li>
-                    </ul>
-                </nav>
+                <br>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- Move the "Log in," "Register," and "Contact" links to the far left -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Log in</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <?php
+                        if (isset($_GET["no_boycott"])) {
+                            echo "<span class='nav-link'>No boycott products</span>";
+                        }
+                        ?>
+                    </li>
+                </ul>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- search part of the page -->
+                    <form class="d-flex ms-auto" method='post' action='search.php'>
+                        <input class="form-control me-2" type="txt" name="search" placeholder="Search" aria-label="Search" id='search'>
+                        <button class="btn btn-outline-light" name="submit" type="Search">Search</button>
+                    </form>
+                    <!-- search part of the page -->
+                </div>
             </div>
         </nav>
-        <!-- second child -->
+
+
+
         <div class="bg-light">
-            <h3 class="text-center p-2">Manage Details</h3>
+            <h3 class="text-center">Andalus Store </h3>
         </div>
 
+        <div class="container">
+            <div class="row">
+            <div class="col-md-9">
+                    <div class="row">
+                        <?php
+                        while ($row = $result->fetch_assoc()) {
+                            $product_id = $row["product_id"];
+                            $product_name = $row["product_name"];
+                            $product_image = $row["product_image"];
 
-        <!-- third child -->
-        <div class="row">
-            <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
-                <div class="p-3">
-                    <a href="#"><img src="../images/one.jpeg" alt="" class="admin_image"></a>
-                </div>
-                <!-- Buttons -->
-                <div class="button text-center mx-1">
-                    <a href="products.php" class="btn btn-primary" ml-6>View Products</a>
+                            echo "<div class='col-md-4 mb-2'>";
+                            echo "<div class='card'>";
+                            echo "<img src='admin_area/uploads/$product_image' class='card-img-top' alt='...'>";
+                            echo "<div class='card-body'>";
+                            echo "<h5 class='card-title'>$product_name</h5>";
+                            echo "<a href='product_details.php?id=$product_id' class='btn btn-info'>View more</a>";
+                            echo "</div>";
+                            echo "</div>";
+                            echo "</div>";
+                        }
+                        ?>
+                    </div>
                 </div>
 
-                <div class="button text-center mx-1">
-                    <a href="view_orders.php" class="btn btn-primary" ml-6>View orders</a>
+
+                <!-- filter part of the page ************************************** -->
+                
+                <div class="col-md-3 bg-secondary p-3">
+                <form class="" method='get' action='filter.php'>
+          <h5>Filter</h4>
+          <!-- No boycott-->
+            <a href="index.php?no_boycott=1" class="btn btn-primary">Remove all boycott products</a>
+
+          <!-- price filter ************************************** -->
+            <div class="mb-2">
+              <label for="priceRange" class="form-label text-light">Price Range:</label>
+              <select class="form-select" id="priceRange" name="priceRange">
+                <option value="priceRange">Select Price Range </option>
+                <?php 
+                while ($row = $result_ranges->fetch_assoc()) {
+                    $min = $row["MIN(price)"];
+                    $max = $row["MAX(price)"];
+                    echo "<option value='$min - $max'>$min - $max</option>";
+                }
+                ?>   
+              </select>
+        <!-- brand filter ************************************** -->  
+            </div>
+            <div class="mb-2">
+              <label for="brandName" class="form-label text-light">Brand Name:</label>
+              <select class="form-select" id="brandName" name="brandName">
+                <option value="brandName">Select Brand Name</option>
+                    <?php
+                        while ($row = $result_brands->fetch_assoc()) {
+                            $row = $row["brand_name"];
+                            echo "<option value='$row'>$row</option>";
+                        }
+                    ?>
+                
+              </select>
+            </div>
+
+        <!-- nationality filter ************************************** -->
+            <div class="mb-2">
+              <label for="brandNationality" class="form-label text-light">Brand Nationality:</label>
+              <select class="form-select" id="brandNationality" name="brandNationality">
+                <option value="brandNationality">Select Nationality</option>
+                    <?php
+                        while ($row = $result_nat->fetch_assoc()) {
+                            $row = $row["nationality"];
+                            echo "<option value='$row'>$row</option>";
+                        }
+                    ?>
+              </select>
+            </div>
+            <div class="mb-2">
+
+              <div class="mb-2">
+                <button type="submit" name='apply_filter' class="btn btn-light">Apply Filter</button>
+                <button type="button" class="btn btn-secondary" onclick="resetFilter()">Reset</button>
+             
+        </form>
                 </div>
             </div>
         </div>
 
-        <!-- last child -->
-<div class="bg-info p-3 text-center footer">
-    <p>Andalus store</p>
-</div>
-</div>
+        <div class="bg-info p-3 text-center">
+            <p>Andalus store</p>
+        </div>
+    </div>
 
-<!-- bootstrap js link -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-crossorigin="anonymous"></script>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+            crossorigin="anonymous">
+    </script>
+
+    <script>
+        function resetFilter() {
+            document.getElementById("priceRange").value = "priceRange";
+            document.getElementById("brandName").value = "brandName";
+            document.getElementById("brandNationality").value = "brandNationality";
+        }
+    </script>
 </body>
 </html>
