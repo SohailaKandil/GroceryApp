e14afb1fa37e2b7b32e4be97c2ce5fc1f43174fe
Alaa Moheb <106888094+alaaamoheb@users.noreply.github.com>
2023-12-31 21:56:28 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -56,22 +56,6 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-custom">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">Andalus Store</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="home.php">Home</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
     <div class="container mt-5">
         <h2 class="mb-4">Contact Us</h2>
 
@@ -103,7 +87,7 @@
                 <label for="message" class="form-label">Your Message:</label>
                 <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
             </div>
-            <button type="submit" class="btn btn-custom">Submit</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 
